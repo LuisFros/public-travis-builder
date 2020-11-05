67daa1c8a709457a885a25df594ad2a95a36e4f3
LuisFros <lmfros@uc.cl>
2020-11-05 10:30:15 -0300

    add appspec for codedeploy

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pwd=$( aws ecr get-login-password )
+docker container stop $(docker container ls -aq)
+docker login -u AWS -p $pwd 564835583352.dkr.ecr.us-east-1.amazonaws.com/backend_web
+docker pull 564835583352.dkr.ecr.us-east-1.amazonaws.com/backend_web
