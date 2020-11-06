b68459c0909020d2fcddf6634026a45db71ca62e
LuisFros <lmfros@uc.cl>
2020-11-06 12:42:06 -0300

    fix(): run scripts from home

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-pwd=$( aws ecr get-login-password )
+pwd=$( aws ecr get-login-password --region us-east-1)
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd 564835583352.dkr.ecr.us-east-1.amazonaws.com/backend_web
 docker pull 564835583352.dkr.ecr.us-east-1.amazonaws.com/backend_web
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 docker-compose -f home/ubuntu/backend/prod-docker-compose.yml down
 docker stop $(docker ps -a -q)
