4d4c01e875a5e93010cb170744477b2fb59bd38d
LuisFros <lmfros@uc.cl>
2020-11-10 11:03:34 -0300

    chore(): release: v2.0.0

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -10,16 +10,28 @@ class SessionsController < ApiController
   def create
     @user = User.find_by(username: params[:username])
     if @user
-      return render json: @user
+      begin
+        resp = Cognito.authenticate({USERNAME: params[:username], PASSWORD: "Password12?"}).authentication_result
+      rescue => e
+        resp = e
+      end
+      session[:access_token] = resp['access_token']
     else
       return render json: {"error": "usuario no encontrado"}
     end
   end
 
   def destroy
+    if request.headers['Authorization']
+      Cognito.sign_out(request.headers['Authorization'])
+      resp = { type: 'success', message: 'now you are disconected' }
+    else
+      resp = { type: 'error', message: 'empty token' }
+    end
     session.delete(:user_id)
     @current_user = nil
     redirect_to '/welcome'
+    return render json: resp
   end
 
   def login
@@ -30,5 +42,4 @@ class SessionsController < ApiController
       redirect_to '/rooms'
     end
   end
-
 end
