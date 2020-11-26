c76b1981ef9539dd90a9e568c30d7fffbc28bbd0
LuisFros <lmfros@uc.cl>
2020-11-26 08:18:32 -0300

    chore(): release 3.0.0

--- backend/spec/requests/rooms_request_spec.rb
+++ backend/spec/requests/rooms_request_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Rooms", type: :request do
+    let(:user) {User.create!}
+
     it "creates a Room" do
         headers = { "ACCEPT" => "application/json" }
-        post "/rooms", :params => { :name => 'TestRoom' }, :headers => headers
+        post "/rooms", :params => { :name => 'TestRoom', :user_id=>user.id }, :headers => headers
     
-        expect(response.content_type).to eq("2pplication/json")
+        expect(response.content_type).to eq("application/json")
         expect(response).to have_http_status(:ok)
       end
 
